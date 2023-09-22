a68f949a200c710dce8d2673235ea132365731a1
Song Hui jun <xhrl233@naver.com>
2023-09-22 19:50:31 +0900

    V6:왼쪽 역삼격형 리팩토링

--- star3.py
+++ star3.py
@@ -1,5 +1,6 @@
-print("*****")
-print(" ****")
-print("  ***")
-print("   **")
-print("    *")
+for i in range(5):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(5-i):
+        print("*",end="")
+    print()
